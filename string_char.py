75b1112fa0fb040fc6b3a2f45f7b5328d21d3254
Ella W Tuominen <ella.w.tuominen@gmail.com>
2025-09-24 20:38:26 +0100

    Add string_char.py file and write function for calculating characters in string

---
+++ string_char.py
@@ -0,0 +1,7 @@
+def count_string_characters(str):
+    count = 0
+    for chr in str:
+        count += 1
+    return count
+
+print(count_string_characters("Hello, nice to meet you! Nice to meet you too!"))  # Output: 13
